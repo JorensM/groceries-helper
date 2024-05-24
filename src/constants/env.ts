3b596744a3fda17643406de7971e22939922a9d4
JorensM <jorensmerenjanu@gmail.com>
2024-05-24 12:49:33 +0300

    add debug log

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,3 +1,5 @@
+console.log('vercel url: ', import.meta.env.VITE_PUBLIC_VERCEL_URL, import.meta.env.VERCEL_URL);
+
 export const APP_URL = import.meta.env.ENVIRONMENT == 'vercel' ? 
     import.meta.env.VITE_PUBLIC_VERCEL_URL : 
     import.meta.env.VITE_PUBLIC_APP_URL;
