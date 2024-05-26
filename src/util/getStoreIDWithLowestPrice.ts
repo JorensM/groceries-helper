162fdd88588900fd35e780c4932a99c2b1d5153c
JorensM <jorensmerenjanu@gmail.com>
2024-05-26 14:53:03 +0300

    fix: getStoreIDWithLowestPrice throwing error if no stores are added

--- src/util/getStoreIDWithLowestPrice.ts
+++ src/util/getStoreIDWithLowestPrice.ts
@@ -1,6 +1,9 @@
 import { Grocery } from '#/types/Grocery';
 
 export default function getStoreIDWithLowestPrice(grocery: Grocery) {
+    if(!Object.keys(grocery.prices).length) {
+        return undefined;
+    }
     return parseInt(Object.entries(grocery.prices).reduce(([prevKey, prevValue], [currKey, currValue]) => {
         if(currValue && prevValue && (currValue || 0) < (prevValue || 0)){
             return [currKey, currValue]
